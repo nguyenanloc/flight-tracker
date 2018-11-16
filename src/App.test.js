b1a30bfaea7aec4c69bffab0b6327f0dc6846cac
An Loc Nguyen <anloc.l.nguyen@boeing.com>
2018-11-16 14:21:43 -0800

    add logo

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe("App",() =>{
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('check for image logo', () => {
+    //setup
+    const appWrapper = shallow(<App/>)
+    const logo = appWrapper.find('#logo')
+
+    //exercise
+
+    //Assert
+    expect(logo).toHaveLength(1)
+
+  });
+})
